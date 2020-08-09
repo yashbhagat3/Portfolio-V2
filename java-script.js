c7f5ebb9fa4d74444376bbfa2982be49e4eadfe1
Yash Bhagat <yashbhagat1999@gmail.com>
2020-08-10 05:09:11 +0530

    .

--- java-script.js
+++ java-script.js
@@ -1,7 +1,32 @@
 $(window).on("load",function(){
     $("#loader").fadeOut("slow");
+    $('dummyanim').first().addClass('reveal');
 
 })
 
+$(window).scroll(function () 
+{
+    var scrolled = $(this).scrollTop();
+    var interval = 0;
+    
+    $('.grid-container').children().each(function () 
+    {
+        var startToShow = $(this).offset().top + $(this).outerHeight()/3; //fires when a third of the div appears, change the 3 to experiment with different positions
+        var bottomOfWindow = $(window).scrollTop() + $(window).height();
+        if (bottomOfWindow > startToShow)
+        {
+            $(this).addClass('reveal');
+        }
+    });
 
+    $('.section').children().each(function () 
+    {
+        var startToShow = $(this).offset().top + $(this).outerHeight()/3; //fires when a third of the div appears, change the 3 to experiment with different positions
+        var bottomOfWindow = $(window).scrollTop() + $(window).height();
+        if (bottomOfWindow > startToShow)
+        {
+            $(this).addClass('reveal');
+        }
+    });
+});
 
