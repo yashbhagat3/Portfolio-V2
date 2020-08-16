e6a9cf11154c7c9f9d6b4583849e80fb1f15db01
Yash Bhagat <yashbhagat1999@gmail.com>
2020-08-16 16:21:49 +0530

    .

--- java-script.js
+++ java-script.js
@@ -1,10 +1,10 @@
 $(window).on("load",function(){
     $("#loader").fadeOut("slow");
     $('dummyanim').first().addClass('reveal');
-
+    $('wave').first().addClass('anim');
     $('.main').children().each(function () 
     {
-        var startToShow = $(this).offset().top + $(this).outerHeight()/3; //fires when a third of the div appears, change the 3 to experiment with different positions
+        var startToShow = $(this).offset().top + $(this).outerHeight()/5; //fires when a third of the div appears, change the 3 to experiment with different positions
         var bottomOfWindow = $(window).scrollTop() + $(window).height();
         if (bottomOfWindow > startToShow)
         {
